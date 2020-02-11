a8af7767eda2203c8f6055886d57914858a81953
s00kh00 <ec2-user@ip-172-31-13-211.us-west-2.compute.internal>
2020-02-11 01:45:01 +0000

    starting solution

--- elegant_solution.py
+++ elegant_solution.py
@@ -5,9 +5,16 @@ print (sys.version)
 
 grid = [[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1],[0,0,0,0]]
 row=grid[0]
-print (grid[0])
-num_row = print(len(row))
+num_column = len(grid)
+num_row = len(row)
+print("Number Column:%s" % num_column)
 print("Number Rows:%s" % num_row)
+
+#row connections
+for x in range(grid)
+    row_sum=row_sum+sum
+    sum=0
+    for y in range(len(row))
 
 # target = 22
 # for x in range(len(A)):
